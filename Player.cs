a321e87e9a74ff6f0cdf8db675dad2b4ea5703b2
Iam_Manis <57586602+cy-Manish@users.noreply.github.com>
2023-03-04 22:03:13 +0545

    Add files via upload

--- Player.cs
+++ Player.cs
@@ -7,7 +7,7 @@ public class Player : KinematicBody2D
     private int gravity = 40;
     private float friction = .1f;
     private float acceleration = .5f;
-    private int jumpHeight = 100;
+    private int jumpHeight = 80;
     private int dashSpeed = 500;
     private bool isDashing = false;
     private float dashTimer = .2f;
@@ -19,145 +19,212 @@ public class Player : KinematicBody2D
     private Vector2 velocity = new Vector2();
     private bool canClimb = true;
     private int climbSpeed = 100;
+    private bool isClimbing = false;
+    private float climbTimer = 5f;
+    private float climbTimerReset = 5f;
+    private bool isInAir = false;
+    // [Export]
+    // public PackedScene GhostPlayerInstance;
 
     // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+    public override void _Ready() { }
+
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
     {
-        
-    }
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
- public override void _Process(float delta)
- {
-    if(!isDashing && !isWallJumping){
-        processMovement(delta);
-
+        if (!isDashing && !isWallJumping)
+        {
+            processMovement(delta);
         }
-        if(IsOnFloor()){
-            if(Input.IsActionJustPressed("jump"))
+        if (IsOnFloor())
+        {
+            if (Input.IsActionJustPressed("jump"))
             {
-                velocity.y = -jumpHeight;  
+                velocity.y = -jumpHeight;
+                 GetNode<AnimatedSprite>("AnimatedSprite").Play("Jump");
+                 isInAir=true;
             }
-            canClimb=true;
+            else{
+                isInAir=false;
+            }
+            canClimb = true;
             isDashAvailable = true;
         }
-        if(canClimb){
+        if (canClimb)
+        {
             processClimb(delta);
         }
-
-
-        processWallJump(delta);  
-
-            
-
-        if(isDashAvailable)
+        if(!IsOnFloor())
         {
-           processDash(delta);
+        processWallJump(delta);
+        }
+        if (isDashAvailable)
+        {
+            processDash(delta);
         }
 
-        if(isDashing){
+        if (isDashing)
+        {
             dashTimer -= delta;
-            if(dashTimer <= 0)
+            GhostPlayer ghost = GhostPlayerInstance.Instance() as GhostPlayer;
+            Owner.AddChild(ghost);
+            ghost.GlobalPosition = this.GlobalPosition;
+            if (dashTimer <= 0)
             {
-                isDashing = false;
-                velocity = new Vector2(0,0);
+                isDashing = false; 
+                velocity = new Vector2(0, 0);
             }
         }
-            else
-            {
-                velocity.y += gravity * delta;
-
+        else if (!isClimbing)
+        {
+            velocity.y += gravity * delta;
+        }else{
+            climbTimer -= delta;
+            if (climbTimer <= 0){
+                isClimbing = false;
+                canClimb = false;
+                climbTimer=climbTimerReset;
             }
+        }
 
-        MoveAndSlide(velocity, Vector2.Up);  
-
+        MoveAndSlide(velocity, Vector2.Up);
     }
 
-    private void processClimb(float delta){
-        if(Input.IsActionPressed("climb")){
-            if(canClimb){
-                if(Input.IsActionPressed("ui_up")){
+    private void processClimb(float delta)
+    {
+        if (
+            Input.IsActionPressed("climb")
+            && (
+                GetNode<RayCast2D>("RayCastLeft").IsColliding()
+                || GetNode<RayCast2D>("RayCastRight").IsColliding()
+                || GetNode<RayCast2D>("RayCastRightClimb").IsColliding()
+                || GetNode<RayCast2D>("RayCastLeftClimb").IsColliding()
+            )
+        )
+        {
+            if (canClimb && !isWallJumping)
+            {
+                isClimbing = true;
+
+                if (Input.IsActionPressed("ui_up"))
+                {
                     velocity.y = -climbSpeed;
                 }
-                else if(Input.IsActionJustPressed("ui_down")){
-                    velocity.y = climbSpeed;
-
-                }
-                else{
-                    velocity = new Vector2(0,0);
-                }
-
-            }
-         }
-    }
-    private void processMovement(float delta){
-        int direction = 0;
-        if(Input.IsActionPressed("ui_left")){
-            direction -= 1;
-        }
-        if(Input.IsActionPressed("ui_right")){
-            direction += 1;
-        }
-        if(direction != 0){
-            velocity.x = Mathf.Lerp(velocity.x, direction * speed, acceleration);
-        }
-        else{
-            velocity.x = Mathf.Lerp(velocity.x, 0, friction);
-        }
-    }
-    private void processDash(float delta){
-         if(Input.IsActionJustPressed("dash"))
-            {
-                if (Input.IsActionPressed("ui_left"))
+                else if (Input.IsActionJustPressed("ui_down"))
                 {
-                    velocity.x = -dashSpeed;
-                    isDashing = true;
-
-                    }
-                    if (Input.IsActionPressed("ui_right")){
-                        velocity.x = dashSpeed;
-                        isDashing = true; 
-
-                    }
-
-                    if (Input.IsActionPressed("ui_up")){
-                        velocity.y = -dashSpeed;
-                        isDashing = true;
-                    }
-
-                    if (Input.IsActionPressed("ui_right") && Input.IsActionPressed("ui_up")){
-                        velocity.x = dashSpeed;
-                        velocity.y = -dashSpeed;
-                        isDashing = true;
-                    }
-                    if (Input.IsActionPressed("ui_left") && Input.IsActionPressed("ui_up")){
-                        velocity.x = -dashSpeed;
-                        velocity.y = -dashSpeed;
-                        isDashing = true;
-                    }
-
-                dashTimer = dashTimerReset;
-                isDashAvailable = false;
-            }
-    }
-    private void processWallJump(float delta){
-        if (Input.IsActionJustPressed("jump") && GetNode<RayCast2D>("RayCastLeft").IsColliding()){
-                velocity.y = -jumpHeight;
-                velocity.x = jumpHeight;
-                isWallJumping = true;
-            }
-            else if (Input.IsActionJustPressed("jump") && GetNode<RayCast2D>("RayCastLeft").IsColliding()){
-                velocity.y = -jumpHeight;
-                velocity.x = -jumpHeight;
-                isWallJumping = true;
-            }
-            if (isWallJumping){
-                wallJumpTimer -= delta;
-                if(wallJumpTimer <= 0){
-                    isWallJumping =false;
-                    wallJumpTimer = wallJumpTimerReset;
+                    velocity.y = climbSpeed;
+                }
+                else
+                {
+                    velocity = new Vector2(0, 0);
                 }
             }
+            else
+            {
+                isClimbing = false;
+            }
+            }
+        else
+        {
+            isClimbing = false;
+        }
     }
 
+    private void processMovement(float delta)
+    {
+        int direction = 0;
+        if (Input.IsActionPressed("ui_left"))
+        {
+            direction -= 1;
+             GetNode<AnimatedSprite>("AnimatedSprite").FlipH=true;
+        }
+        if (Input.IsActionPressed("ui_right"))
+        {
+            direction += 1;
+             GetNode<AnimatedSprite>("AnimatedSprite").FlipH =false;
+        }
+        if (direction != 0)
+        {
+            velocity.x = Mathf.Lerp(velocity.x, direction * speed, acceleration);
+            if(!isInAir){
+                GetNode<AnimatedSprite>("AnimatedSprite").Play("Run");
+            }
+        }
+        else
+        {
+            if(!isInAir){
+                 GetNode<AnimatedSprite>("AnimatedSprite").Play("Front");
+                 velocity.x = Mathf.Lerp(velocity.x, 0, friction);
+            }
+        }
+    }
+
+    private void processDash(float delta)
+    {
+        if (Input.IsActionJustPressed("dash"))
+        {
+            if (Input.IsActionPressed("ui_left"))
+            {
+                velocity.x = -dashSpeed;
+                isDashing = true;
+            }
+            if (Input.IsActionPressed("ui_right"))
+            {
+                velocity.x = dashSpeed;
+                isDashing = true;
+            }
+
+            if (Input.IsActionPressed("ui_up"))
+            {
+                velocity.y = -dashSpeed;
+                isDashing = true;
+            }
+
+            if (Input.IsActionPressed("ui_right") && Input.IsActionPressed("ui_up"))
+            {
+                velocity.x = dashSpeed;
+                velocity.y = -dashSpeed;
+                isDashing = true;
+            }
+            if (Input.IsActionPressed("ui_left") && Input.IsActionPressed("ui_up"))
+            {
+                velocity.x = -dashSpeed;
+                velocity.y = -dashSpeed;
+                isDashing = true;
+            }
+
+            dashTimer = dashTimerReset;
+            isDashAvailable = false;
+        }
+    }
+
+    private void processWallJump(float delta)
+    {
+        if (Input.IsActionJustPressed("jump") && GetNode<RayCast2D>("RayCastLeft").IsColliding())
+        {
+            velocity.y = -jumpHeight;
+            velocity.x = jumpHeight;
+            isWallJumping = true;
+             GetNode<AnimatedSprite>("AnimatedSprite").FlipH=false;
+        }
+        else if (
+            Input.IsActionJustPressed("jump") && GetNode<RayCast2D>("RayCastLeft").IsColliding()
+        )
+        {
+            velocity.y = -jumpHeight;
+            velocity.x = -jumpHeight;
+            isWallJumping = true;
+            GetNode<AnimatedSprite>("AnimatedSprite").FlipH=false;
+
+        }
+        if (isWallJumping)
+        {
+            wallJumpTimer -= delta;
+            if (wallJumpTimer <= 0)
+            {
+                isWallJumping = false;
+                wallJumpTimer = wallJumpTimerReset;
+            }
+        }
+    }
 }
